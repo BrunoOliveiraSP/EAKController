556587d140211bcd0356bc73c1ca8ed278a97cd3
Bruno de Oliveira <brunooliveira.me@outlook.com>
2020-03-05 08:29:30 -0300

    camadas

--- Backend/API/Database/PontoDatabase.cs
+++ Backend/API/Database/PontoDatabase.cs
@@ -2,6 +2,12 @@ namespace API.Database
 {
     public class PontoDatabase
     {
-        
+        Models.eakdbContext db = new Models.eakdbContext();
+
+        public void Inserir (Models.TbPonto ponto)
+        {
+            db.TbPonto.Add(ponto);
+            db.SaveChanges();
+        }
     }
 }
