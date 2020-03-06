5ab17dfa1a8eda26cc27c01e928be6be050443aa
Bruno de Oliveira <brunooliveira.me@outlook.com>
2020-03-06 15:36:58 -0300

    crud do ponto feito, falta o controller

--- Backend/API/Business/PontoBusiness.cs
+++ Backend/API/Business/PontoBusiness.cs
@@ -1,7 +1,54 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 namespace API.Business
 {
     public class PontoBusiness
     {
-        
+        Database.PontoDatabase db = new Database.PontoDatabase();
+        public void Inserir(Models.TbPonto ponto)
+        {
+            if(ponto.DsObservacao.Trim() == null)
+            {
+                throw new ArgumentException("A observação é obrigatória");
+            }
+
+            db.Inserir(ponto);
+        }
+        public void Deletar(int id)
+        {
+            if(id == 0)
+            throw new ArgumentException("Ponto não encontrado");
+
+            db.Deletar(id);
+        }
+        public void Alterar(Models.TbPonto ponto)
+        {
+            if(ponto.DsObservacao.Trim() == null)
+            {
+                throw new ArgumentException("A observação é obrigatória");
+            }
+
+            db.Alterar(ponto);
+        }
+        public List<Models.TbPonto> ListarTodos()
+        {
+            List<Models.TbPonto> lista = null;
+
+            lista = db.ListarTodos();
+            return lista;
+        }
+        public List<Models.TbPonto> Consultar(int idserie)
+        {
+            if(idserie == 0)
+            throw new ArgumentException("Registro não encontrado");
+
+            List<Models.TbPonto> lista = null;
+
+            lista = db.Consultar(idserie);
+            return lista;
+        }
     }
 }
--- Backend/API/Database/PontoDatabase.cs
+++ Backend/API/Database/PontoDatabase.cs
@@ -1,6 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
 namespace API.Database
 {
     public class PontoDatabase
     {
+        Models.eakdbContext db = new Models.eakdbContext();
+        
+        public void Inserir(Models.TbPonto ponto)
+        {
+            db.TbPonto
+              .Add(ponto);
+
+            db.SaveChanges();
+        }
+        public List<Models.TbPonto> Consultar(int idserie)
+        {
+            List<Models.TbPonto> lista = null;
+
+            lista = db.TbPonto
+                      .Where(t=>t.IdSerieAluno == idserie)
+                      .ToList();
+
+            return lista;
+        }
+        public void Deletar(int id)
+        {
+            Models.TbPonto del = null;
+
+            del = db.TbPonto
+                    .FirstOrDefault(t=>t.IdPonto == id);
+
+            db.Remove(del);
+
+            db.SaveChanges();
+        }
+        public void Alterar(Models.TbPonto ponto)
+        {
+            Models.TbPonto novo = null;
+
+            novo = db.TbPonto
+                     .FirstOrDefault(t=>t.IdPonto == ponto.IdPonto);
+
+            novo.QtPontos = ponto.QtPontos;
+            novo.DsObservacao = ponto.DsObservacao;
+            novo.DtAdicionado = ponto.DtAdicionado;
+
+            db.SaveChanges();
+        }
+        public List<Models.TbPonto> ListarTodos()
+        {
+            List<Models.TbPonto> lista = null;
+
+            lista = db.TbPonto
+                      .ToList();
+                      
+            return lista;
+        }
+
+
     }
 }
