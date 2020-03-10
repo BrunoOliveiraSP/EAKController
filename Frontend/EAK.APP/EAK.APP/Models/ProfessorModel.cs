54953aea0c727e90cae35508f2573afbd5b5cf90
Bruno de Oliveira <brunooliveira.me@outlook.com>
2020-03-10 15:29:29 -0300

    ProfessorApi e DIsciplina Api feitas

---
+++ Frontend/EAK.APP/EAK.APP/Models/ProfessorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAK.APP.Models
+{
+    class ProfessorModel
+    {
+        public int IdProfessor { get; set; }
+        public string NmProfessor { get; set; }
+        public string  DsEmail { get; set; }
+        public string DsSenha { get; set; }
+    }
+}
